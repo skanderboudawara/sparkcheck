7a6931ba3b6ca4a25e7a550ed6bf0e981f8c3d22
Skander Boudawara <108568261+skanderboudawara@users.noreply.github.com>
2025-01-12 14:58:30 +0100

    remove jsonpath dependency

--- src/sparkchecker/ext/_decorators.py
+++ src/sparkchecker/ext/_decorators.py
@@ -223,7 +223,11 @@ def check_dataframe(method: Callable) -> Callable:
                 f"{class_name}: The target must be a Spark DataFrame, "
                 f"but got '{type(target).__name__}'.",
             )
-        if target.isEmpty():
+        if (
+            # Add this checks only for Column Class
+            class_name.lower().startswith("col")
+            and target.isEmpty()
+        ):
             return {
                 "has_failed": False,
                 "got": "Empty DataFrame",
