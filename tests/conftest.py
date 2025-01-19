530f8c973789ac2fc69566b10a3616bd82c69c3f
Skander Boudawara <108568261+skanderboudawara@users.noreply.github.com>
2025-01-19 03:27:02 +0100

    remove unnecessary Spark configuration settings in spark_session fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,11 +6,6 @@ import pytest
 def spark_session():
     spark = (
         SparkSession.builder.appName("pytest")
-        .config("spark.sql.shuffle.partitions", 1)
-        .config("spark.num.executors", 1)
-        .config("spark.executor.cores", 1)
-        .config("spark.executor.memory", "512m")
-        .config("spark.driver.memory", "512m")
         .getOrCreate()
     )
     spark.catalog.clearCache()
